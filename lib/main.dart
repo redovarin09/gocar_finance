32a148d3849683cf46f49cde5f63855600ac3e99
redovarin09 <redovarin@gmail.com>
2026-06-20 15:42:27 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'app/app.dart';
 
 void main() {
   runApp(const ProviderScope(child: GocarFinanceApp()));
-}
-
-class GocarFinanceApp extends StatelessWidget {
-  const GocarFinanceApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GocarFinance',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF00880D),
-        ),
-        useMaterial3: true,
-      ),
-      home: const _SplashPlaceholder(),
-    );
-  }
-}
-
-class _SplashPlaceholder extends StatelessWidget {
-  const _SplashPlaceholder();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF5F5F5),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 88,
-              height: 88,
-              decoration: BoxDecoration(
-                color: const Color(0xFF00880D),
-                borderRadius: BorderRadius.circular(22),
-              ),
-              child: const Icon(
-                Icons.directions_car_rounded,
-                color: Colors.white,
-                size: 52,
-              ),
-            ),
-            const SizedBox(height: 24),
-            const Text(
-              'GocarFinance',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF1A1A1A),
-                letterSpacing: -0.5,
-              ),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'Pencatatan Keuangan Driver GoCar',
-              style: TextStyle(
-                fontSize: 14,
-                color: Color(0xFF616161),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
