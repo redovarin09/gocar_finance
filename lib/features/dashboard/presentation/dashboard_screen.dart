d51b6d0297aef7b93a4d39f82d9f3a5668578da3
redovarin09 <redo.varin@gmail.com>
2026-06-21 10:22:32 +0700

    fix: missing repository imports + go_router import

--- lib/features/dashboard/presentation/dashboard_screen.dart
+++ lib/features/dashboard/presentation/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_text_styles.dart';
 import '../../../shared/providers/app_providers.dart';
