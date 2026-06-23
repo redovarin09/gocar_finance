bd582faed7780b12d3b5fdee75887cc1d9448d5f
redovarin09 <redo.varin@gmail.com>
2026-06-23 19:43:47 +0700

    fix: add WidgetsFlutterBinding.ensureInitialized()

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: GocarFinanceApp()));
 }
